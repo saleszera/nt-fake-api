eb7d79c7cc5bc47e4da706be42ab7aa1b9bd0ac1
saleszera <crowzcoleslaw@gmail.com>
2021-04-22 17:37:40 -0300

    adequando a fake api com a api final

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const db = require('./db.json');
 
 server.use(middlewares);
 server.use(router);
+
+server.use(jsonServer.rewriter({
+  '/api/website/news': '/news'
+}));
+server.use(jsonServer.rewriter({
+  '/api/website/home': '/home'
+}));
+server.use(jsonServer.rewriter({
+  '/api/website/forecasts': '/forecasts'
+}));
+server.use(jsonServer.rewriter({
+  '/api/lightnings/state': '/state'
+}));
+
+
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON server is running');
+  console.log('🚀️ JSON server is running 🚀️');
 });
+
